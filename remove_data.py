47f40cbd299033f81da23dca81ebe4428a4e20db
Beau Smit <40701137+Beau-Smit@users.noreply.github.com>
2021-11-30 16:20:35 -0600

    clean food data

--- remove_data.py
+++ remove_data.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 
 def missing_at_random(data, perc_remove, rand_seed):
     np.random.seed(rand_seed)
@@ -14,12 +14,14 @@ def missing_at_random(data, perc_remove, rand_seed):
 
 
 if __name__ == "__main__":
-    sample_data = np.array([[4, 7, 1, 9, 6],
-                        [1, 7, 6, 5, 9],
-                        [7, 0, 3, 4, 4],
-                        [2, 4, 8, 7, 8],
-                        [7, 2, 1, 1, 6]])
-                        
+    # sample_data = np.array([[4, 7, 1, 9, 6],
+    #                     [1, 7, 6, 5, 9],
+    #                     [7, 0, 3, 4, 4],
+    #                     [2, 4, 8, 7, 8],
+    #                     [7, 2, 1, 1, 6]])
+
+    sample_data = pd.read_csv("nutrition_data/NutritionalFacts_Fruit_Vegetables_Seafood.csv")
+
     print(sample_data)
     new_data = missing_at_random(sample_data, perc_remove=0.6, rand_seed=25)
     print(new_data)
