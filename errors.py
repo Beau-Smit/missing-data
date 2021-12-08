e183a88f15400e8dc7e8bb9b1d97b2f0d22bfd69
Beau Smit <40701137+Beau-Smit@users.noreply.github.com>
2021-12-08 16:27:28 -0600

    add feature to plot another line on same graph

--- errors.py
+++ errors.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def line_plot(missingness, errors):
+def line_plot(missingness, errors1, errors2=None):
     plt.figure()
-    plt.plot(missingness, errors, c='#9D2EC5')
-    plt.title("How is error affected by missingness?")
-    plt.xlabel("proportion missing")
-    plt.ylabel("error")
+    plt.plot(missingness, errors1, c='#9D2EC5')
+    if errors2 is None:
+        pass
+    else:
+        plt.plot(missingness, errors2, c='#841a06')
+    plt.title("Error rate vs missingness")
+    plt.xlabel("Proportion of data missing")
+    plt.ylabel("Reconstruction error (Frobenius norm)")
     plt.grid(True)
     plt.show()
     # plt.savefig("output/lineplot_errors.png")
