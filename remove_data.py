b2dca2bbd057f2088287d0a3a3d5bb390814529c
Beau Smit <40701137+Beau-Smit@users.noreply.github.com>
2021-12-10 10:48:12 -0600

    change conditional missingness functions

--- remove_data.py
+++ remove_data.py
@@ -6,6 +6,7 @@ import math
 def missing_at_random(data, perc_remove, rand_seed):
     np.random.seed(rand_seed)
 
+    perc_remove = perc_remove / 100
     idx_arr = np.concatenate([np.repeat(0, perc_remove * data.size), 
                             np.repeat(1, (1-perc_remove) * data.size)])
     discrep = idx_arr.size != data.size
@@ -19,24 +20,66 @@ def missing_at_random(data, perc_remove, rand_seed):
     return random_missing
 
 
-def missing_conditional_continuous(data_original, target_col, reference_col, threshold, steepness = .001):
+def missing_conditional_continuous(data_original, target_col, reference_col, percent_missing, high_missing=False):
+    """Note: only use this after normalization
+    """
     data = data_original.copy()
+
+    # create probability for each entry being missing
+    threshold = np.percentile(data[reference_col], percent_missing)
     p_missing = data[reference_col].apply(
-            lambda x: 1/(1+ math.e**(-steepness*(x - threshold)))
+            lambda x: 1/(1+ math.e**(-100*(x - threshold)))
         )
-    target_col_mask = p_missing.apply(
-        lambda x: random.choices([False,True], weights=[x,1], k = 1)[0]
-    )
+    
+    random_percent_missing = 10000
+    # if not within 2 percent of our missing goal, try again
+    while abs(random_percent_missing - percent_missing) > 2:
+        # remove entries based on probabilities
+        if high_missing == True:
+            target_col_mask = p_missing.apply(
+                lambda x: random.choices([False,True], weights=[x, 1-x], k = 1)[0]
+            )
+        else:
+            target_col_mask = p_missing.apply(
+                lambda x: random.choices([False,True], weights=[1-x, x], k = 1)[0]
+            )
+        random_percent_missing = (1 - target_col_mask.mean()) * 100
+
     data.loc[:,target_col] = data[target_col][target_col_mask]
+
     return data
 
 
-def missing_conditional_discrete(data_original, target_col, reference_col, reference_likelihoods):
+# def missing_conditional_discrete(data_original, target_col, reference_col, reference_likelihoods):
+#     data = data_original.copy()
+#     target_col_mask = data[reference_col].apply(
+#         lambda x: random.choices([False,True], weights=[reference_likelihoods[x],1], k = 1)[0]
+#     )
+#     data.loc[:,target_col] = data[target_col][target_col_mask]
+#     return data
+
+def missing_conditional_discrete(data_original, target_col, percent_missing):
     data = data_original.copy()
-    target_col_mask = data[reference_col].apply(
-        lambda x: random.choices([False,True], weights=[reference_likelihoods[x],1], k = 1)[0]
-    )
-    data.loc[:,target_col] = data[target_col][target_col_mask]
+    
+    value_prevalence = (data['Discrete_LowLikelihood_0'].value_counts() / data.shape[0]).sort_values()
+
+    # choose level of missingess for less frequent value
+    missing_lvl_1 = np.random.random()
+    
+    # remove proper amount of those values
+    column_mask = data[target_col].apply(
+            lambda x: random.choices([False,True], weights=[missing_lvl_1, 1-missing_lvl_1], k = 1)[0] if x==value_prevalence.index[0] else np.nan)
+
+    # choose level of missingness for more frequent value so that total percent missing is correct
+    entries_removing = (column_mask.loc[column_mask.notnull()] == False).sum()
+    entries_left_to_remove = int((data.shape[0] * percent_missing/100) - entries_removing)
+
+    # choose as many indexes as needed to make the right number missing
+    indices_to_remove = random.choices(column_mask.loc[column_mask.isnull()].index, k=entries_left_to_remove)
+
+    data.loc[indices_to_remove, target_col] = np.nan
+    data.loc[column_mask == False, target_col] = np.nan
+    
     return data
 
 
